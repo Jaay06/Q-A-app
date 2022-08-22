568f3d8214a8a534794ac982f3e3435a8e4c0d04
Jaay06 <josekoyi9@gmail.com>
2022-08-22 15:18:57 +0100

    needs a final touch

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,30 +1,168 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+// import { v4 } from 'uuid'
 
-export default function Trivia() {
-  useEffect(() => {
-    function questions() {
-      fetch('https://opentdb.com/api.php?amount=10')
-        .then((res) => res.json())
-        .then((data) => setData(data.results))
+export default function Trivia(props) {
+  const [questions, setQuestions] = useState([extractQuestions()])
+  const [correctAnswers, setCorrectAnswers] = useState([
+    extractCorrectAnswers(),
+  ])
+  const [allAnswers, setAllAnswers] = useState([extractAllAnswers()])
+  const [count, setCount] = useState(0)
+  const [submit, setSubmit] = useState(false)
+  const [tryAgain, setTryAgain] = useState(true)
+  const [playAgain, setPlayAgain] = useState(true)
+  const [heldAnswers, setHeldAnswers] = useState([])
+
+  function extractQuestions() {
+    const allQuestions = props.dataArray
+    const newArray = []
+    for (let i = 0; i < 5; i++) {
+      const array = {
+        id: i + 1,
+        question: allQuestions[i].question,
+        answer: [
+          ...allQuestions[i].incorrect_answers,
+          allQuestions[i].correct_answer,
+        ].sort(() => (Math.random() > 0.5 ? 1 : -1)),
+      }
+      newArray.push(array)
     }
-    questions()
-  }, [])
+    return newArray
+  }
 
-  const [data, setData] = useState({})
+  function extractCorrectAnswers() {
+    const allQuestions = props.dataArray
+    const newArray = []
+    for (let i = 0; i < 5; i++) {
+      const array = {
+        id: i + 1,
+        answer: allQuestions[i].correct_answer,
+        // isCorrect: true,
+      }
+      newArray.push(array)
+    }
+    return newArray
+  }
 
-  const triviaArray = data.map((trivia) => {
-    return (
-      <div>
-        <h5>{trivia.question}</h5>
-        <div className="answers">
-          <small className="answer">{trivia.correct_answer}</small>
-          <small className="answer">{trivia.incorrect_answers[0]}</small>
-          <small className="answer">{trivia.incorrect_answers[1]}</small>
-          <small className="answer">{trivia.incorrect_answers[2]}</small>
+  //   const allIncoorect = props.dataArray[0].incorrect_answers
+
+  function extractAllAnswers() {
+    const answers = props.dataArray
+    const newArray = []
+    for (let i = 0; i < 5; i++) {
+      const array = {
+        id: i + 1,
+        answer: [
+          ...answers[i].incorrect_answers,
+          answers[i].correct_answer,
+        ].sort(() => (Math.random() > 0.5 ? 1 : -1)),
+      }
+      newArray.push(array)
+    }
+    return newArray
+  }
+
+  //Counter
+  function increment() {
+    setCount((prev) => prev + 1)
+  }
+
+  //   const styles = {
+
+  //   }
+  //Rendered DOM element
+  const element = questions.map((e) =>
+    e.map((i) => {
+      return (
+        <div key={i.id}>
+          <h5>{i.question}</h5>
+          <div className="answers">
+            {i.answer.map((z) => {
+              return (
+                <button
+                  disabled={false}
+                  key={z + 1}
+                  className="answer"
+                  onClick={(event) => handleClick(event, i.id)}
+                  // style={{
+                  //   backgroundColor: i.id === heldAnswers.id
+                  // }}
+                >
+                  {z}
+                </button>
+              )
+            })}
+          </div>
         </div>
-      </div>
-    )
-  })
+      )
+    }),
+  )
 
-  return <div>{triviaArray}</div>
+  // console.log(correctAnswers)
+
+  //Click event that checks for answers and sets Count
+  function handleClick(event, id) {
+    //Selects the correct answer
+    for (const [a, b, c, d, e] of correctAnswers) {
+      if (id === a.id) {
+        event.target.classList.toggle('bg-color')
+        a.answer === event.target.innerText && increment()
+      } else if (id === b.id) {
+        event.target.classList.toggle('bg-color')
+        b.answer === event.target.innerText && increment()
+      } else if (id === c.id) {
+        event.target.classList.toggle('bg-color')
+        c.answer === event.target.innerText && increment()
+      } else if (id === d.id) {
+        event.target.classList.toggle('bg-color')
+        d.answer === event.target.innerText && increment()
+      } else if (id === e.id) {
+        event.target.classList.toggle('bg-color')
+        e.answer === event.target.innerText && increment()
+      }
+
+      console.log(event.target.disabled)
+    }
+    //Holds the answer
+    // const answer = {
+    //   id: id,
+    //   value: event.target.innerText,
+    //   held: true,
+    // }
+
+    // setHeldAnswers([...heldAnswers, answer])
+  }
+
+  function check() {
+    setSubmit(true)
+    setTryAgain(false)
+  }
+
+  // function restart() {
+  //   setSubmit(false)
+  //   setTryAgain(true)
+  //   setCount()
+  //   play()
+  // }
+
+  function play() {
+    setPlayAgain(false)
+    setPlayAgain(true)
+  }
+
+  return (
+    <div className="trivia">
+      {<main>{element}</main>}
+      {tryAgain ? (
+        <button className="btns" onClick={check}>
+          Check Answers
+        </button>
+      ) : (
+        <button className="btns" onClick={() => window.location.reload(false)}>
+          Click to reload and restart
+        </button>
+      )}
+      {submit && <p className="bottom__text">You scored {count}/5</p>}
+    </div>
+  )
 }
---
+++ src/components/array.js
@@ -0,0 +1,102 @@
+const array = [
+  {
+    category: 'History',
+    type: 'multiple',
+    difficulty: 'medium',
+    question:
+      'Which country had an &quot;Orange Revolution&quot; between 2004 and 2005?',
+    correct_answer: 'Ukraine',
+    incorrect_answers: ['Belarus', 'Latvia', 'Lithuania'],
+  },
+  {
+    category: 'Animals',
+    type: 'multiple',
+    difficulty: 'hard',
+    question: 'Which of these animals is NOT a lizard?',
+    correct_answer: 'Tuatara',
+    incorrect_answers: ['Komodo Dragon', 'Gila Monster', 'Green Iguana'],
+  },
+  {
+    category: 'Mythology',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'According to the Egyptian Myth of Osiris, who murdered Osiris?',
+    correct_answer: 'Set',
+    incorrect_answers: ['Horus', 'Ra', 'Anhur'],
+  },
+  {
+    category: 'Entertainment: Video Games',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      'In Heroes of the Storm, the Cursed Hollow map gimmick requires players to kill the undead to curse the enemy team.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    category: 'History',
+    type: 'boolean',
+    difficulty: 'hard',
+    question:
+      'During the Winter War, the amount of Soviet Union soliders that died or went missing was five times more than Finland&#039;s.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Entertainment: Video Games',
+    type: 'multiple',
+    difficulty: 'hard',
+    question:
+      'In &quot;Magicka&quot;, which two elements are combined to cast the spell, &quot;Nullify&quot;?',
+    correct_answer: 'Arcane + Shield',
+    incorrect_answers: [
+      'Lightning + Shield',
+      'Lightning + Life',
+      'Arcane + Life',
+    ],
+  },
+  {
+    category: 'General Knowledge',
+    type: 'multiple',
+    difficulty: 'medium',
+    question:
+      'The website &quot;Shut Up &amp; Sit Down&quot; reviews which form of media?',
+    correct_answer: 'Board Games',
+    incorrect_answers: ['Television Shows', 'Video Games', 'Films'],
+  },
+  {
+    category: 'Entertainment: Music',
+    type: 'multiple',
+    difficulty: 'hard',
+    question:
+      'What is the first track on the Dave Matthews Band album &quot;Before These Crowded Streets&quot;?',
+    correct_answer: 'Pantala Naga Pampa',
+    incorrect_answers: [
+      'Stay (Wasting Time)',
+      'Rapunzel',
+      'Don&#039;t Drink The Water',
+    ],
+  },
+  {
+    category: 'Entertainment: Music',
+    type: 'multiple',
+    difficulty: 'easy',
+    question:
+      'What 1970&#039;s American ballad referred to the 1959 plane crash as the &quot;the day the music died&quot;?',
+    correct_answer: 'American Pie',
+    incorrect_answers: [
+      'Kentucky Rain',
+      'I Will Always Love You',
+      'Rock &#039;n&#039; Roll Suicide',
+    ],
+  },
+  {
+    category: 'General Knowledge',
+    type: 'boolean',
+    difficulty: 'medium',
+    question:
+      'The commercial UK channel ITV stands for &quot;International Television&quot;.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+]
